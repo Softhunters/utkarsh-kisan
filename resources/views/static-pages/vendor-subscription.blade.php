8a804831d8e5eae5ae0d55d9fc4613fc156c76ee
manvi59 <manviyogi59@gmail.com>
2025-07-16 15:46:44 +0530

    banner background image added , product detail, category detail page name fixes, subscription responsive

--- resources/views/static-pages/vendor-subscription.blade.php
+++ resources/views/static-pages/vendor-subscription.blade.php
@@ -3,17 +3,12 @@
 
 
 @section('content')
-
-
  
-
-
-
   <div class="container">
    
      <div class="h1-service-area pt-40 mb-40">
         <div class="container">
-            <div class="row">
+            <div class="row g-5">
                 <div class="col-lg-12 d-flex justify-content-center">
                     <div class="section-title1 text-center">
                         <span> <img src="{{ asset('assets/images/icon/section-vec-l1.svg') }}"
@@ -23,8 +18,8 @@
                     </div>
                 </div>
             </div>
-            <div class="row" style="padding-top:20px">
-                <div class="col-4">
+            <div class="row g-4" style="padding-top:20px">
+                <div class="col-lg-4 col-xl-4 col-md-6  col-sm-12 col-12">
                     <div class="services-card1">
                         <img class="services-card-vec mark-logo"
                             src="{{ asset('/assets/images/icon/mark-logo.svg') }}" alt="" />
@@ -34,7 +29,7 @@
                         </div>
                         <div class="content">
                             <h3><a href="#">1-Month Plan </a></h3>
-                            <h4><a href="#">Price - ₹250 </a></h4>
+                            <h4><a href="#" class="subscription_price">Price - ₹250 </a></h4>
 
                             <p>Perfect for Small Businesses Use only 250 INR and discover our platform. It is perfect when there is a small business, which wants to test the waters, get access to new customers and grow without long term commitment and without the pressure of the occasion.</p>
                         </div>
@@ -42,7 +37,7 @@
                                 src="{{ asset('assets/images/icon/btn-arrow1.svg') }}" alt="" /></a> -->
                     </div>
                 </div>
-                <div class="col-4">
+                <div class="col-lg-4  col-xl-4 col-md-6 col-sm-12  col-12">
                     <div class="services-card1">
                         <img class="services-card-vec mark-logo"
                             src="{{ asset('/assets/images/icon/mark-logo.svg') }}" alt="" />
@@ -52,14 +47,14 @@
                         </div>
                         <div class="content">
                             <h3><a href="#">3-Month Plan </a></h3>
-                            <h4><a href="#">Price - ₹500 </a></h4>
+                            <h4><a href="#" class="subscription_price">Price - ₹500 </a></h4>
                             <p>Build Trust and Gain Customers To make your brand time and connect with more buyers, you have to spend just 500 (rupees). An intelligent decision to increase awareness, interaction and customer loyalty within 3 months.</p>
                         </div>
                         <!-- <a class="more-btn" href="#">More Details<img
                                 src="{{ asset('assets/images/icon/btn-arrow1.svg') }}" alt="" /></a> -->
                     </div>
                 </div>
-                <div class="col-4">
+                <div class="col-lg-4 col-xl-4 col-md-6 col-sm-12 col-12">
                     <div class="services-card1">
                         <img class="services-card-vec mark-logo"
                             src="{{ asset('/assets/images/icon/mark-logo.svg') }}" alt="" />
@@ -69,7 +64,7 @@
                         </div>
                         <div class="content">
                             <h3><a href="#">6-Month Plan</a></h3>
-                            <h4><a href="#">Price - ₹800 </a></h4>
+                            <h4><a href="#" class="subscription_price">Price - ₹800 </a></h4>
                             <p>Maximize Sales, Minimize Cost Extend your presence in India and enhance long-term performance at only 800 rupees. A plan that gives regular visibility and sales at a cost that is less than a cup of tea per day.</p>
                         </div>
                         <!-- <a class="more-btn" href="#">More Details<img
